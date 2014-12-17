4d31d218eb3ae455eb5a8e01a32586fe030b535e
super-yan <alvindaiyan@gmail.com>
2014-12-17 21:07:28 +1300

    add connection to mongodb, not tested yet

---
+++ src/main/java/com/datesc/util/SDException.java
@@ -0,0 +1,12 @@
+package com.datesc.util;
+
+/**
+ * Created by Yan on 12/17/2014.
+ */
+public class SDException extends Exception
+{
+    public SDException(String message)
+    {
+        super(message);
+    }
+}
